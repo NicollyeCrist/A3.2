1d94720fed1495412109875bd3a88313a4a4d0f2
NicollyeCrist <128860826+NicollyeCrist@users.noreply.github.com>
2023-11-28 20:31:04 -0300

    Create autenticarcnpj.php

---
+++ autenticarcnpj.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+include('validarlogin.php');
+
+$cnpj = isset($_POST['cnpj']) ? $_POST['cnpj'] : '';
+
+$url = file_get_contents('https://www.receitaws.com.br/v1/cnpj/'.$cnpj);
+
+$dados = json_decode($url);
+var_dump($dados);
+
+?>
